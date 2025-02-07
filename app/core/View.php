004ad39507b3f8c421e87de12433bf9d12632d2c
ismailbaoud <ibaoud46@gmail.com>
2025-02-07 03:14:29 -0500

    ajouter la class de view pour gere l'fichage des twig

--- app/core/View.php
+++ app/core/View.php
@@ -1,8 +1,36 @@
-<?php 
+<?php
 namespace app\core;
 
-class   View{
-    
-}
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
-?>
+class View {
+    private $twig;
+
+    public function __construct() {
+        $loader = new FilesystemLoader(dirname(__DIR__) . '/views');
+        $this->twig = new Environment($loader, [
+            'cache' => false,
+            'debug' => true,
+            'auto_reload' => true
+        ]);
+
+        // Add the asset function
+        $this->twig->addFunction(new \Twig\TwigFunction('asset', function ($path) {
+            return '/assets/' . ltrim($path, '/');
+        }));
+
+        // Add the url function
+        $this->twig->addFunction(new \Twig\TwigFunction('url', function ($path) {
+            return '/' . ltrim($path, '/');
+        }));
+    }
+
+    public function render($template, $data = []) {
+        // Add user data to all views if user is logged in
+        if (isset($_SESSION['user'])) {
+            $data['user'] = $_SESSION['user'];
+        }
+        echo $this->twig->render($template, $data);
+    }
+}
